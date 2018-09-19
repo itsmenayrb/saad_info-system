8a03e6ad3e8af3d8fe9f940447f09f21e3f1176c
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-09-19 12:53:00 +0800

    Added ids to elements

--- login.php
+++ login.php
@@ -3,23 +3,23 @@
 ?>
 	<div class="main-container">
 		<div class="login-container">
-			<p> Login </p>
-			<form action="includes/login.inc.php" method="post">
-				<div>
+			<p id="login-page-title"> Login </p>
+			<form id="login-form" action="includes/login.inc.php" method="post">
+				<div id="login-username-input">
 					<span id="username_login_checked"></span>
 					<input type="text" name="username" id="username" placeholder="Username or Email" required/>
 				</div>
-				<div>
+				<div id="login-password-input">
 					<span id="password_login_checked"></span>
 					<input type="password" name="password" id="password" placeholder="Password" required/>
 				</div>
-				<div>
+				<div id="login-submit-button">
 					<input type="submit" name="submit" id="submit" value="Login"/>
 				</div>
-				<div>
+				<div id="login-register-link">
 					Not a member? <a href="register.php">Sign Up</a>
 				</div>
-				<div>
+				<div id="login-account-reset-link">
 					<br><a href="forgotpassword.php">Forgot Password.</a>
 				</div>			
 			</form>
