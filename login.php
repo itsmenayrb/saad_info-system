080eb4c016d1b5ecd6b22ed520dc9eda1d6308ac
Bryan <bryan.balaga@gmail.com>
2018-09-19 09:55:52 +0800

    Added "required" function on all inputs

--- login.php
+++ login.php
@@ -7,11 +7,11 @@
 			<form action="includes/login.inc.php" method="post">
 				<div>
 					<span id="username_login_checked"></span>
-					<input type="text" name="username" id="username" placeholder="Username or Email" onkeyup="check_username_login()" />
+					<input type="text" name="username" id="username" placeholder="Username or Email" required/>
 				</div>
 				<div>
 					<span id="password_login_checked"></span>
-					<input type="password" name="password" id="password" placeholder="Password"/>
+					<input type="password" name="password" id="password" placeholder="Password" required/>
 				</div>
 				<div>
 					<input type="submit" name="submit" id="submit" value="Login"/>
@@ -21,8 +21,7 @@
 				</div>
 				<div>
 					<br><a href="forgotpassword.php">Forgot Password.</a>
-				</div>
-				
+				</div>			
 			</form>
 		</div>
 	</div>
--- register.php
+++ register.php
@@ -6,22 +6,22 @@
 			<p> Register </p>
 			<form action="includes/register.inc.php" method="post">
 				<div>
-					<input type="text" name="fname" id="fname" placeholder="First Name" onkeyup="checkfname();"/>
+					<input type="text" name="fname" id="fname" placeholder="First Name" onkeyup="checkfname();" required/>
 				</div>
 				<div>
-					<input type="text" name="lname" id="lname" placeholder="Last Name" onkeyup="checklname();"/>
+					<input type="text" name="lname" id="lname" placeholder="Last Name" onkeyup="checklname();" required/>
 				</div>
 				<div>
-					<input type="text" name="email" id="email" placeholder="Email" onkeyup="checkemail();"/>
+					<input type="text" name="email" id="email" placeholder="Email" onkeyup="checkemail();" required/>
 				</div>
 				<div>
-					<input type="text" name="username" id="username" placeholder="Username" onkeyup="checkusername();"/>
+					<input type="text" name="username" id="username" placeholder="Username" onkeyup="checkusername();" required/>
 				</div>
 				<div>
-					<input type="password" name="password" id="password" placeholder="Password" onkeyup="checkpassword();"/>
+					<input type="password" name="password" id="password" placeholder="Password" onkeyup="checkpassword();" required/>
 				</div>
 				<div>
-					<input type="password" name="cpassword" id="cpassword" placeholder="Retype Password" onkeyup="checkcpassword();"/>
+					<input type="password" name="cpassword" id="cpassword" placeholder="Retype Password" onkeyup="checkcpassword();" required/>
 				</div>
 				<div>
 					<input type="submit" name="submit" id="submit" value="Register"/>
