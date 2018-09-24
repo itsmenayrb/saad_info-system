5ffa4aebee74f4682b15c26ba635fcfcd11995cb
Renz Christen Yeomer A Pagulayan <larongbingo@gmail.com>
2018-09-24 07:13:42 +0000

    Replaced container-fluid to container

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 		<link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
 
 		<nav class="navbar navbar-default">
-		  <div class="container-fluid">
+		  <div class="container">
 		    <!-- Brand and toggle get grouped for better mobile display -->
 		    <div class="navbar-header">
 		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
