17a3b680877003c3b82754921a62bbac4bfa20c1
Bryan <bryan.balaga@gmail.com>
2018-10-02 18:59:07 +0800

    Updated Database

--- testing.sql
+++ testing.sql
@@ -16,33 +16,64 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`barangaysalitranii` /*!40100 DEFAULT CH
 
 USE `barangaysalitranii`;
 
+/*Table structure for table `address` */
+
+DROP TABLE IF EXISTS `address`;
+
+CREATE TABLE `address` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `block` varchar(255) NOT NULL,
+  `street` varchar(255) NOT NULL,
+  `subdivision` varchar(255) NOT NULL,
+  `barangay` varchar(255) DEFAULT 'Salitran II',
+  `city` varchar(255) DEFAULT 'Dasmariñas',
+  `province` varchar(255) DEFAULT 'Cavite',
+  `country` varchar(255) DEFAULT 'Philippines',
+  `zipcode` int(11) DEFAULT '4114',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `address` */
+
 /*Table structure for table `residents` */
 
 DROP TABLE IF EXISTS `residents`;
 
 CREATE TABLE `residents` (
   `user_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `LastName` varchar(256) NOT NULL,
+  `Prefix` varchar(255) NOT NULL,
   `FirstName` varchar(256) NOT NULL,
-  `MiddleName` varchar(256),
-  `Suffix` varchar(256),
-  `Bday` date,
-  `Bplace` varchar(256),
-  `Address` varchar(500),
-  `ContactNumber` int(11),
-  `Email` varchar(256) NOT NULL,
-  `Username` varchar(256) NOT NULL,
-  `Password` varchar(256) NOT NULL,
-  `SecQuestion1` varchar(256),
-  `SecQuestion2` varchar(256),
+  `MiddleName` varchar(256) NOT NULL,
+  `LastName` varchar(256) NOT NULL,
+  `Suffix` varchar(256) NOT NULL,
+  `Sex` varchar(255) NOT NULL,
+  `Bday` date NOT NULL,
+  `Bplace` varchar(256) NOT NULL,
+  `Address` varchar(256) NOT NULL,
+  `TelephoneNumber` bigint(11) NOT NULL,
+  `CellphoneNumber` bigint(11) NOT NULL,
   PRIMARY KEY (`user_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*Data for the table `residents` */
 
-insert  into `residents`(`user_ID`,`LastName`,`FirstName`,`MiddleName`,`Suffix`,`Bday`,`Bplace`,`Address`,`ContactNumber`,`Email`,`Username`,`Password`,`SecQuestion1`,`SecQuestion2`) values 
-(8,'Balaga','Bryan','','','0000-00-00','','',0,'123@gmail.com','brybry','$2y$10$HQmpI2EM1J/rMm8U/itaReCQxqTMqUmJj1n7kFzHoOyVM6RDFen7O','',''),
-(9,'Balaga','Bryan','','','0000-00-00','','',0,'asdasd','bryan','$2y$10$gk.Ttg23Kay6p6auf.YNsORjEbrTX/ItePbrxny2zoYu/bBsbDx7W','','');
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Username` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `SecurityQuestion1` varchar(255) NOT NULL,
+  `SecurityQuestion2` varchar(255) NOT NULL,
+  `Token` varchar(255) NOT NULL,
+  `TokenSum` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
