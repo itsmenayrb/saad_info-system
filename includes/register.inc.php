002f0017978b31995edf6d7f207019f556d44fb5
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-10-09 13:10:09 +0800

    Moved dateChecking to functions.php

--- includes/register.inc.php
+++ includes/register.inc.php
@@ -3,22 +3,7 @@
 include "dbh.inc.php";
 //dbh.inc.php yung filename ng connector sa database, $conn naman yung variable.
 
-/**
- * Check if a string is a valid date(time)
- *
- * DateTime::createFromFormat requires PHP >= 5.3
- * 
- * Source: https://www.pontikis.net/tip/?id=21
- *
- * @param string $str_dt
- * @param string $str_dateformat
- * @param string $str_timezone (If timezone is invalid, php will throw an exception)
- * @return bool
- */
-function isValidDateTimeString($str_dt, $str_dateformat, $str_timezone) {
-  $date = DateTime::createFromFormat($str_dateformat, $str_dt, new DateTimeZone($str_timezone));
-  return $date && DateTime::getLastErrors()["warning_count"] == 0 && DateTime::getLastErrors()["error_count"] == 0;
-}
+include "../functions.php";
 
 if (isset($_POST['submit'])) {
 
