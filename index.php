ae6a0505be101a02dc0683f0a0948f499ecc4739
Bryan <bryan.balaga@gmail.com>
2018-10-11 17:17:07 +0800

    Re-design GUI

--- index.php
+++ index.php
@@ -1,47 +1,170 @@
-<?php include 'header.php'; ?>
+<?php
+    include 'includes/dbh.inc.php';
+    include 'header.php';
+?>
 
-<div class="main-container">
-	<?php
-			if (isset($_SESSION['id'])) {
-				echo "<h1>You are logged in</h1>";
-			}
-	?>
-	
-	<div class="container" style="width:100%;">
-		<!-- Logo and Banner -->
-		<div style="width:100%;background-color: #0f72b8;height:150px;">
-				<img src="logo.png" alt="logo" height="130" width="540" style="margin-top:10px;margin-left:50px;"/>			
-		</div>
-		<div id="myCarousel" class="carousel slide" data-ride="carousel">
-		  <!-- Indicators -->
-		  <ol class="carousel-indicators">
-		    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-		    <li data-target="#myCarousel" data-slide-to="1"></li>
-		    <li data-target="#myCarousel" data-slide-to="2"></li>
-		  </ol>
-		
-		  <!-- Wrapper for slides -->
-		  <div class="carousel-inner" style="height:500px;">
-		    <div class="item active">
-		      <img src="http://1.bp.blogspot.com/-XehccLXiIrY/U9RkEPDEfzI/AAAAAAAAAGI/zLeF-nD8jVs/s1600/10577071_1444145629197316_3578534339715827921_n.jpg" alt="Los Angeles" style="width:100%;height:500px;">
-		    </div>
-		
-		    <div class="item">
-		      <img src="http://1.bp.blogspot.com/-4pLC3WsNxRo/U9RkIOwtv0I/AAAAAAAAAGc/ZRUlty9d9oM/s1600/1451456_1444145622530650_8604715179296023340_n.jpg" alt="Chicago" style="width:100%;height:500px">
-		    </div>
-		  </div>
-		
-		  <!-- Left and right controls -->
-		  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-		    <span class="glyphicon glyphicon-chevron-left"></span>
-		    <span class="sr-only">Previous</span>
-		  </a>
-		  <a class="right carousel-control" href="#myCarousel" data-slide="next">
-		    <span class="glyphicon glyphicon-chevron-right"></span>
-		    <span class="sr-only">Next</span>
-		  </a>
-		</div>
-	</div>
-</div>
+<body>
+    <div class="carousel slide carousel-fade" id="myCarousel" data-ride="carousel"> <!-- CAROUSEL -->
+        <ol class="carousel-indicators">
+            <li data-target="myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="myCarousel" data-slide-to="1"></li>
+            <li data-target="myCarousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox"> <!-- CAROUSEL INNER -->
+            <div class="carousel-item active">
+                <img src="img/hotlines.jpg" class="img-fluid" alt="hotlines">
+                <div class="carousel-caption d-none d-md-block" id="carouselCaption1">
+                    <h2 class="text-justify"><strong>Be equipped!</strong><br><br>Save these numbers to your mobile phone.</h2>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="img/medical.jpg" class="img-fluid" alt="medical">
+                <div class="carousel-caption d-none d-md-block" id="carouselCaption2">
+                    <h2 class="text-justify"><strong>Medical<br>on the Go!</strong></h2><br><br>
+                    <a class="btn btn-primary btn-lg pull-left" role="button" href="index.php#medical">Learn more.</a>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="img/assembly.jpg" class="img-fluid" alt="assembly">
+                <div class="carousel-caption d-none d-md-block" id="carouselCaption3">
+                    <h2><strong>Yey!<br>It's General Assembly</strong></h2><br>
+                    <a class="btn btn-primary btn-lg pull-left" role="button" href="index.php#assembly">Learn more.</a>
+                </div>
+            </div>
+        </div> <!-- END OF CAROUSEL INNER -->
+        <!-- CONTROLS -->
+        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div> <!-- END OF CAROUSEL -->
+    <!-- CARDS -->
+    <div class="jumbotron jumbotron-fluid" id="newsWrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="card-deck">
+                        <div class="card">
+                            <img class="card-img-top" src="img/hotlines.jpg" alt="Hotline Numbers">
+                            <div class="card-body">
+                                <h5 class="card-title">Hotline Numbers</h5>
+                                <p class="card-text">Be sure that always ready. Save these numbers to your mobile phone.</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <img class="card-img-top" src="img/medical.jpg" alt="Medical">
+                            <div class="card-body">
+                                <h5 class="card-title">Medical</h5>
+                                <p class="card-text">Murang Medikal handog ng barangay.</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">Last updated 1 day ago</small>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <img class="card-img-top" src="img/assembly.jpg" alt="Barangay Assembly">
+                            <div class="card-body">
+                                <h5 class="card-title">Barangay Assembly</h5>
+                                <p class="card-text lead">Makialam! Makilahok! at Makiisa!</p>
+                                <p class="card-text">Nagkakaisang Komunidad tungo sa Mapayapa, Maunlad at Matiwasay na Barangay</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">Last updated 3 days ago</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-md-auto">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="card" style="width:18rem;">
+                                <img class="card-img-top" src="..." alt="Construction">
+                                <div class="card-body">
+                                    <p class="card-text">Lorem ipsum Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                        Integer posuere erat a ante.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-auto">
+                            <div class="card" style="width:18rem;">
+                                <img class="card-img-top" src="..." alt="Construction">
+                                <div class="card-body">
+                                    <p class="card-text">Lorem ipsum Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                        Integer posuere erat a ante.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        <!-- END OF CARDS -->
+    <!-- MISSION AND VISION -->
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <div class="carousel">
+                <small class="text-muted text-center">
+                    MISSION AND VISION
+                </small>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="carousel-caption d-none d-md-block">MISSION</div>
+                        <p style="font-family: 'Century Gothic';"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        $(function () {
+            $(".carousel").carousel({
+                interval: 5000,
+                pause: true,
+                keyboard: true
+            });
+        });
 
-<?php include 'footer.php';?>
+        $(function(){
+            $('#resetselector').change(function(){
+                $('.thru').hide();
+                $('#' + $(this).val()).show();
+            });
+        });
+
+        var  email = $("#remail");
+
+        $(document).ready(function () {
+            $('#submit').on('click', function(){
+                if(email.val() != ""){
+                    email.css('border', '1px solid green');
+
+                    $.ajax({
+                        url:'forgotpassword.php',
+                        method: 'POST',
+                        dataType: 'text',
+                        data: {
+                            email: email.val()
+                        }, success: function (response){
+                            console.log(response);
+                        }
+                    })
+                }
+                else{
+                    email.css('border', '1px solid red');
+                }
+            });
+        });
+    </script>
+</body>
+<?php
+    include 'footer.php';
+?>
+
